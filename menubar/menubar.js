ab7b5439a5e2f3298167c576b8bb0fa268c74fa6
DhirajHolkar11 <143429479+DhirajHolkar11@users.noreply.github.com>
2023-10-01 17:38:15 +0530

    Add files via upload

---
+++ menubar/menubar.js
@@ -0,0 +1,23 @@
+function loginPage(){
+    window.location.href = "loginPage.html";
+}
+
+function signupPage(){
+    window.location.href = "signupPage.html";
+}
+
+function homePage(){
+    window.location.href = "homePage.html";
+}
+
+function toolsPage(){
+    window.location.href = "toolsPage.html";
+}
+
+function aboutPage(){
+    window.location.href = "aboutPage.html";
+}
+
+function contactPage(){
+    window.location.href = "contactPage.html";
+}
